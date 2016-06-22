b7a3b8186744ead4bc372b92c1abb95dc272cffc
michellewkx <371640895@qq.com>
2016-06-22 14:39:46 +0800

    finish booklist page

--- app/assets/javascripts/Controllers/bookList/booklist.component.js
+++ app/assets/javascripts/Controllers/bookList/booklist.component.js
@@ -2,7 +2,7 @@
 * @Author: michellewkx
 * @Date:   2016-06-21 16:25:12
 * @Last Modified by:   michellewkx
-* @Last Modified time: 2016-06-21 20:40:58
+* @Last Modified time: 2016-06-22 12:02:15
 */
 
 'use strict';
@@ -12,10 +12,10 @@ angular.module("bookList", []);
 angular.
   module('bookList').
   component('bookList', {
-    templateUrl: '/public/book-list.html',
+    templateUrl: '/book-list.html',
     controller: ['$http', function BookListController($http) {
       var self = this;
-      $http.get('books/').then(function(response) {
+      $http.get('/books').then(function(response) {
         self.books = response.data;
       });
     }]
