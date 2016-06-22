ccc0fde735e09817ad69ee3ce4570387f5a35547
michellewkx <371640895@qq.com>
2016-06-22 19:17:01 +0800

    finish books datail

--- app/assets/javascripts/Controllers/app.config.js
+++ app/assets/javascripts/Controllers/app.config.js
@@ -2,14 +2,14 @@
 * @Author: michellewkx
 * @Date:   2016-06-21 17:24:45
 * @Last Modified by:   michellewkx
-* @Last Modified time: 2016-06-22 10:29:40
+* @Last Modified time: 2016-06-22 14:54:05
 */
 
 'use strict';
 
 angular.module('bookApp', [
   'ngRoute',
-  // 'bookDetail',
+  'bookDetail',
   'bookList'
 ]);
 
--- app/assets/javascripts/Controllers/bookDetail/bookdetail.component.js
+++ app/assets/javascripts/Controllers/bookDetail/bookdetail.component.js
@@ -2,7 +2,7 @@
 * @Author: michellewkx
 * @Date:   2016-06-21 19:35:21
 * @Last Modified by:   michellewkx
-* @Last Modified time: 2016-06-21 19:40:44
+* @Last Modified time: 2016-06-22 15:41:34
 */
 
 'use strict';
@@ -10,9 +10,14 @@ angular.module('bookDetail',[]);
 
 angular.module('bookDetail')
 		.component('bookDetail',{
-			template:'Detail view for <span>{{$ctrl.bookId}}</span>',
-			controllers:['$routeParams',
-			function BookDetailController($routeParams) {
-				this.bookId = $routeParams.bookId;
+			templateUrl:'book-detail.html',
+			controller:['$routeParams', '$http',
+			function BookDetailController($routeParams,$http) {
+				var self = this;
+				self.bookId = $routeParams.bookId;
+				$http.get("books/" + $routeParams.bookId).then(function(response) {
+					self.bookDetail = response.data;
+				})
 			}]
 		})
+
