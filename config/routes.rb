f40f9427148b648d71fd95295e6e68561eeb62b0
Draveness <stark.draven@gmail.com>
2016-06-22 11:40:52 +0800

    Complete sign up api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  post 'signup' => 'users#create'
+  resources :users, :only => [:create, :show], :defaults => { :format => :json }
   resources :books, :only => [:index, :show], :defaults => { :format => :json }
 
   root to: 'application#angular'
